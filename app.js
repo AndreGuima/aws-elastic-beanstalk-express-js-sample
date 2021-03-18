ae73a6ff21b3527b6fa5e1069e3d22836a73e72e
ANDRE DE PAIVA GUIMARAES <andre.paiva@embraer.com.br>
2021-03-18 11:24:12 -0300

    Mudando o conteudo do arquivo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! André de Paiva Guimarães running on AWS'));
+app.get('/', (req, res) => res.send('Hello World! Andr� e Fernanda running on AWS'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
